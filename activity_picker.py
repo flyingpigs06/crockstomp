cdc7a42a1699d441b6b2843c1cd65c82bcbc2718
flyingpigs06 <mayacharkids@gmail.com>
2016-08-26 17:47:59 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,11 +1,17 @@
-# import specail libaries already built in python
-import random 
-# list of options to select from
-possible_activities = ['a', 'b', 'c']
+# import special libraries already built in python
+import random, urllib2
 
-# choice of what are going to do
-the_activity = random.choice (possible_activities)
+# list of options to select from
+the_url = 'https://raw.githubusercontent.com/bellcodo/congenial-octo-meme/master/activities.lst'
+list_raw_text = urllib2.urlopen(the_url).read()
+
+# print "DEBUG: " + str(list_raw_text.split())
+
+possible_activities = list_raw_text.split()
+
+# choice of what we are going to do
+the_activity = random.choice(possible_activities)
 
 # display the results to the end user
-print "possible activities are: " + str(possible_activities) 
-print "what we are going to do: " + the_activity
+print "Possible activities are: " + str(possible_activities)
+print "What we are going to do: " + the_activity
