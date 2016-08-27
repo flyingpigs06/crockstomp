45d0f76edbc87d97e61ee7f641ca93a7b5ee0cfd
flyingpigs06 <mayacharkids@gmail.com>
2016-08-26 17:22:03 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,4 +1,11 @@
-# import special libraries already built in python
+# import specail libaries already built in python
+import random 
+# list of options to select from
+possible_activities = ['a', 'b', 'c']
 
-# list of options to select from 
-posssible_activities=
+# choice of what are going to do
+the_activity = random.choice (possible_activities)
+
+# display the results to the end user
+print "possible activities are: " + str(possible_activities) 
+print "what we are going to do: " + the_activity
